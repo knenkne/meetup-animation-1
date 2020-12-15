b712c0694c3cbd697ce0b5b77ef9ec5688ec1a9e
KuznetsovMA <jztenk@gmail.com>
2020-12-15 19:49:18 +0300

    init

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import lottie from 'lottie-web'
+import HolyJSON from './Holy.json'
+
+const entry = document.querySelector('.root')
+
+const lottieSettings = {
+    container: entry,
+    renderer: 'canvas',
+    loop: false,
+    autoplay: false,
+    animationData: HolyJSON,
+    rendererSettings: {
+        preserveAspectRatio: 'xMidYMax slice',
+        progressiveLoad: true
+        
+    }
+}
+
+window.addEventListener('resize', lottie.resize)
+
+const lottieInstance = lottie.loadAnimation(lottieSettings)
+lottieInstance.play()
