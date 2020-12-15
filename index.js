76e4a7eceb1f73759f72ed4bd0296b538b80885e
KuznetsovMA <jztenk@gmail.com>
2020-12-15 20:33:53 +0300

    index js

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-var express = require('express');
-var app = express();
+const PORT = process.env.PORT || 5000
 
-app.use(express.static('dist'));
+var express = require('express');
+
+
+express()
+  .use(express.static('dist'))
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
