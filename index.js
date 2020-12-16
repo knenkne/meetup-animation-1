941856e17f9d9a736b972c84a838ed3fc24e2155
KuznetsovMA <jztenk@gmail.com>
2020-12-16 12:34:33 +0300

    init

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 const PORT = process.env.PORT || 5000
 
-var express = require("express")
+var express = require('express');
+
 
 express()
-  .get("/*", (req, res) =>
-    res.sendFile(path.join(__dirname, "dist", "index.html"))
-  )
-  .listen(PORT, () => console.log(`Listening on ${PORT}`))
+  .use(express.static('dist'))
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
